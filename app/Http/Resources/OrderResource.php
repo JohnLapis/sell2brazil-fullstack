ffbda96ea59fe375c5e217e5af7fd7bae8cbd85e
john <borges-2014@live.com>
2022-04-25 08:48:26 -0300

    fix: Calculate values correctly

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -19,7 +19,7 @@ class OrderResource extends JsonResource
             'OrderId' => $this->id,
             'OrderCode' => substr($date, 0, strlen($date)-3) . '-' . $this->id,
             'OrderDate' => $date,
-            'TotalAmountWithoutDiscount' => $this->total - $this->discount,
+            'TotalAmountWithoutDiscount' => $this->total + $this->discount,
             'TotalAmountWithDiscount' => $this->total,
         ];
     }
